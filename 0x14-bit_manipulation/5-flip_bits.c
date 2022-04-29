7d24f1c3c12004d2196d566e2ebe0fd6e23c9b43
Bino26 <hbino1999@gmail.com>
2022-04-28 19:11:30 -0700

    Trying Check for Other task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * clear_bit - set value of bit to 0 at a given index
+ * @n: pointer of unsigned int
+ * @index: index of the bit
+ *
+ * Return: 1 on sucees, -1 on failure
+ *
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int k;
+
+
+	for (k = 0; n || index; n >>= 1, m >>= 1)
+		if ((n & 1) != (m & 1))
+			k++;
+
+
+	return (k);
+}
